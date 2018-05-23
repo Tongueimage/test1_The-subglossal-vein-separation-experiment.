862f9cc4440b37102fd1ef7fe1e4110b63b617cd
whybfq <13588119112@163.com>
2018-05-23 21:24:28 +0800

    Update Different_graphics.m

--- Matlab/Different_graphics.m
+++ Matlab/Different_graphics.m
@@ -5,7 +5,7 @@
 % To draw different graphics
 %  1. Neddle figure
 %  2. pie figure
-% 
+%  3. Three-dimensional curve 
 %Record of revisions:
 %Date           Programmer          Description of change
 %=====          ==============      ===========================
@@ -30,3 +30,18 @@ explode = [0 1 0 0 0];
 pie(data,explode);
 title('\bfExample of  a Pie Plot');
 legend('One ', 'Two','Three','Four','Five');
+
+% Three-dimensional curve
+% x(t)=e^(-0.2t) * cos(2t)
+% y(t) = e^(-0.2t) * sin(2t)
+% 函数表示二维机械系统振动衰退情况，x,y代表在时刻t系统位置。
+t = 0:0.1:10;
+x=exp(-0.2*t).*cos(2*t);
+y = exp(-0.2*t).*sin(2*t);
+plot3(x,y,t);
+title('\bfThree-Dimensional LIne Plot');
+xlabel('\bfx');
+ylabel('\bfy');
+zlabel('\bfTime');
+axis square;
+grid on;
