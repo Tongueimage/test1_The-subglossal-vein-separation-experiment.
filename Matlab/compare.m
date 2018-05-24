2af800c6302d1a6b04ee601d60cef1da8a62a4a2
whybfq <13588119112@163.com>
2018-05-24 11:51:03 +0800

    Create compare.m

---
+++ Matlab/compare.m
@@ -0,0 +1,99 @@
+% Script file: compare.m
+%
+%Purpose:
+% To compare binary and formatted I/O operations.
+% This program generates an array of 10,000 random values and
+% write it to disk both as a binary and as a formatted file.
+% 
+%Record of revisions:
+%Date           Programmer          Description of change
+%=====          ==============      ===========================
+%24-May-2018                        Original
+% 
+%Define variables:
+%count      --Number of values read/written
+%fid        --File id
+%in_array   --input array
+%msg        --Output array
+%out_array  --Operation status
+%time       --Elapsed time in seconds
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Generate the data array.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+out_array = randn(1,10000);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% First ,time the binary output operation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Reset timer
+tic;
+% Loop for 10 times
+for ii = 1:10
+    % Open the binary output file for writing.
+    [fid,msg] = fopen('unformatted.dat','w');
+    % Write the data
+    count = fwrite(fid,out_array,'float64');
+    % Close the file
+    status = fclose(fid);
+end
+% Get the average time
+time = toc/10;
+fprintf('Write time for unformatted file = %6.3f\n',time);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Next, time the formatted output operation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Reset timer
+tic;
+% Loop for 10 times
+for ii = 1:10
+    % Open the formatted output file for writing.
+    [fid,msg] = fopen('formatted.dat','wt');
+    % Write the data
+    count = fprintf(fid, '%23.15e\n',out_array);
+    % Close the file
+    status = fclose(fid);
+end
+% Get the average time
+time = toc/10;
+fprintf('Write time for formatted file = %6.3f\n',time);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Time the binary input operation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Reset timer
+tic;
+%Loop for 10 times;
+for ii = 1:10
+    % Open the binarh file for reading.
+    [fid,msg] = fopen('unformatted.dat','r');
+    % Read the data
+    [in_array,count] = fread(fid,Inf,'float64');
+    % Close the file
+    status = fclose(fid);
+end
+% Get the average time
+time = toc/10;
+fprintf('Read time for unformatted file = %6.3f\n',time);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Time the formatted input operation.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Reset timer
+tic;
+% Loop for 10 times
+for ii = 1:10
+    % Open the formatted file for reading.
+    [fid,msg] = fopen('formatted.dat','rt');
+    % Read the data
+    [in_array, count]=fscanf(fid,'%f',Inf);
+    % Clost the file
+    status = fclose(fid);
+end
+% Get the average time
+time = toc / 10;
+fprintf('Read time for formatted file = %6.3f\n',time)
+
+
+
+Output:
+Write time for unformatted file =  0.001
+Write time for formatted file =  0.011
+Read time for unformatted file =  0.000
+Read time for formatted file =  0.009
